91242f8e10e4058968ea87cc37fbea4e7d2642f6
Gardn <gardn769@gmail.com>
2022-11-01 11:16:58 +0300

    +send mail with qr code

--- src/certificate/certificate.service.ts
+++ src/certificate/certificate.service.ts
@@ -1,12 +1,12 @@
 import { encrypt, decrypt } from "../utils/crypt";
 import { generateQR } from "../utils/qr-generate";
-// import { sendMail } from "../utils/send-mail";
+import { sendMail } from "../utils/send-mail";
 
 export async function createCertificate(data: any) {
   const encryptId = encrypt(data.id); //должна быть строка
   const url = `${encryptId}`;
   await generateQR(url);
-  //   sendMail();
+  await sendMail("gardn769@gmail.com", "rofl");
 }
 
 export function acceptCertificate(encryptId: string) {
--- src/utils/send-mail.ts
+++ src/utils/send-mail.ts
@@ -1,21 +1,27 @@
-// import nodemailer from "nodemailer";
+import nodemailer from "nodemailer";
 
 export async function sendMail(recipient: string, message: string) {
-    // let transporter = nodemailer.createTransport({
-    //     service: mailService,
-    //     host: 'smtp.mail.ru',
-    //     auth: {
-    //         user: userMail, // generated ethereal user
-    //         pass: passMail, // generated ethereal password
-    //     },
-    // });
-    //
-    // let info = await transporter.sendMail({
-    //     from: userMail,
-    //     to: recipient,
-    //     subject: 'message',
-    //     text: message,
-    //     html: "<b>Hello world?</b>",
-    // });
-}
+  let transporter = nodemailer.createTransport({
+    service: "smtp.mail.ru",
+    host: "smtp.mail.ru",
+    auth: {
+      user: "usermail", // generated ethereal user
+      pass: "userpass", // generated ethereal password
+    },
+  });
 
+  let info = await transporter.sendMail({
+    from: "usermail",
+    to: recipient,
+    subject: "message",
+    // text: message,
+    html: message,
+    attachments: [
+      {
+        // filename and content type is derived from path
+        filename: "foo.png",
+        path: "foo.png",
+      },
+    ],
+  });
+}
