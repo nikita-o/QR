80d7b76f60e9e77b13362b4483f13518ba8cb142
Nikita <nik.oschepkov@yandex.ru>
2023-02-07 14:06:08 +0300

    qr через base64

--- src/utils/qr.util.ts
+++ src/utils/qr.util.ts
@@ -1,6 +1,7 @@
 import QRCode from "qrcode";
 
-export function generateQR(data: string): Promise<string> {
-  return QRCode.toString(data, {type: 'svg', width: 192});
+export async function generateQR(data: string): Promise<string> {
+  return (await QRCode.toBuffer(data)).toString('base64');
+  //return QRCode.toString(data, {type: 'svg', width: 192});
   //return QRCode.toBuffer(data);
 }
