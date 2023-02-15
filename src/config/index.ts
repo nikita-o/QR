6fafe96eb27d77d657ac84a0c6c223e8417b299a
Nikita <nik.oschepkov@yandex.ru>
2023-02-15 10:45:46 +0300

    + токен сбера

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,19 +4,16 @@ import fs from "fs";
 const { env } = process;
 export const HTTP_PORT = Number(env.HTTP_PORT) || 3000;
 export const HTTP_HOST = env.HTTP_HOST || "localhost";
+export const hostFront = env.HTTP_HOST_FRONT || 'http://localhost:8081';
+
 export const email = env.EMAIL;
 export const emailPass = env.EMAIL_PASS;
 
 export const TEST = Boolean(Number(env.TEST));
-
 export const urlSberPayment = TEST ? 'https://3dsec.sberbank.ru/payment/rest' : 'https://securepayments.sberbank.ru/payment/rest';
+export const sberToken = env.SBER_TOKEN;
 export const sberLogin = env.SBER_LOGIN;
 export const sberPass = env.SBER_PASS;
-export const hostFront = env.HTTP_HOST_FRONT || 'http://localhost:8081';
-
-
-
-
 
 const keys = JSON.parse(fs.readFileSync("src/config/key.json", "utf8"));
 export const securityKey = Buffer.from(keys.securityKey.data);
--- src/utils/sberbank.util.ts
+++ src/utils/sberbank.util.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {HTTP_HOST, sberLogin, sberPass, urlSberPayment} from "../config";
+import { HTTP_HOST, sberLogin, sberPass, sberToken, urlSberPayment } from "../config";
 import * as https from "https";
 import fs from "fs";
 
@@ -9,51 +9,38 @@ const httpsAgent = new https.Agent({
     ]
 });
 
-export function registerCertificate(orderNumber: string, amount: number) {
-    console.log(sberLogin);
-    console.log(sberPass);
-    console.log(urlSberPayment);
-    console.log(HTTP_HOST);
-    return axios.post(`${urlSberPayment}/register.do`, {
-        userName: sberLogin,
-        password: sberPass,
+export async function registerCertificate(orderNumber: string, amount: number) {
+    const { data } = await axios.post(`${urlSberPayment}/register.do`, {
+        // userName: sberLogin,
+        // password: sberPass,
+        token: sberToken,
         returnUrl: `${HTTP_HOST}/accept-buy-certificate`,
         amount,
         orderNumber,
     }, {
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         httpsAgent,
-    }).then(data => data.data)
-      .catch(err => {
-          console.log(err);
-      });
+    });
+
+    console.log(data);
+
+    return data;
 }
 
 export async function checkStatusCertificate(orderNumber: string): Promise<void> {
-    const orderStatus: number = await axios.post(`${urlSberPayment}/getOrderStatusExtended.do`, {
-        userName: sberLogin,
-        password: sberPass,
+    const { data } = await axios.post(`${urlSberPayment}/getOrderStatusExtended.do`, {
+        // userName: sberLogin,
+        // password: sberPass,
+        token: sberToken,
         orderNumber,
     }, {
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         httpsAgent,
-    }).then(data => data.data.orderStatus);
+    });
 
-    switch (orderStatus) {
-        case 0:
-            throw new Error("заказ зарегистрирован, но не оплачен");
-        case 1:
-            throw new Error("предавторизованная сумма удержана (для двухстадийных платежей)");
-        // 2 - хорошо
-        case 3:
-            throw new Error("авторизация отменена");
-        case 4:
-            throw new Error("по транзакции была проведена операция возврата");
-        case 5:
-            throw new Error("инициирована авторизация через сервер контроля доступа банка-эмитента");
-        case 6:
-            throw new Error("авторизация отклонена");
-        default:
-            throw new Error("авторизация отклонена");
+    console.log(data);
+
+    if (data.orderStatus !== 2) {
+        throw new Error('Error!');
     }
 }
